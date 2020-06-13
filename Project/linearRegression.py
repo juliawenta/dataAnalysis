d03bda4512ed2a66bcea4424daea27c9b8231252
szal03 <33811775+szal03@users.noreply.github.com>
2020-06-13 18:52:36 +0200

    linear regression
    linear regression for CO

---
+++ Project/linearRegression.py
@@ -0,0 +1,188 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#functions
+def correlationFunction(x, y, timeInHours=0):
+    return x.corr(y.shift(timeInHours))
+
+# n - number of shifts
+def checkShift(x, y, n):
+    correlationX = [correlationFunction(x, y, timeInHours=i) for i in range(n)]
+    newArray = pd.DataFrame(list(correlationX)).reset_index()
+    newArray.rename(columns={0: 'correlation', 'index': 'Shift_num'}, inplace=True)
+    #print("")
+    newArray['correlationFix'] = newArray['correlation'].abs()
+    shift = newArray.loc[newArray['correlationFix'] == newArray['correlationFix'].max(), 'Shift_num']
+    tmpShift = shift.to_frame()
+    shift = tmpShift.Shift_num.max()
+
+    return shift
+
+#We are now creating a new DataFrame with a 12-hour shift
+def shiftDataFrame12(airData, target=None, timeInHours=0):
+    newSchift = {}
+    if not timeInHours and not target:
+        return airData
+    for i in airData.columns:
+        if i == target:
+            newSchift[i] = airData[target]
+        else:
+            newSchift[i] = airData[i].shift(periods=timeInHours)
+    #return new dataFrame
+    return  pd.DataFrame(data=newSchift)
+
+#open file
+airData = pd.read_csv('data/AirQuality_Cleared.csv', sep=";")
+
+#Date and Time are objects as we check in clearData.py
+airData['dateAndTime'] = airData['Date']+' '+airData['Time']
+airData['dateAndTime'].head()
+#print(airData['dateAndTime'].head())
+# now we have date and time together
+airData['dateAndTime'] = pd.to_datetime(airData.dateAndTime, format='%d/%m/%Y %H.%M.%S')
+airData.dtypes
+
+#preparing data
+airData['month'] = airData['dateAndTime'].dt.month
+airData['weekday'] = airData['dateAndTime'].dt.weekday
+airData['weekdayName'] = airData['dateAndTime'].dt.day_name()
+airData['hours'] = airData['dateAndTime'].dt.hour
+
+airDataCorrel = airData.corr()
+hourlyAverageCO = airDataCorrel['CO(GT)'].to_frame().sort_values('CO(GT)')
+print(hourlyAverageCO)
+#plot for hourlyAverageCO
+plt.figure(figsize=(10,8))
+hourlyAverageCO.plot(kind='barh', color='cyan')
+plt.title('Correlation with the resulting variable: CO ', fontsize=20)
+plt.xlabel('Correlation level')
+plt.ylabel('Continuous independent variables')
+plt.savefig("Correlation_CO(GT).png")
+plt.show()
+
+x = airData.RH       # independent variable
+y = airData['CO(GT)']    # dependent variable
+N = 20           # number of shifts who will be checked
+
+checkShiftRH = checkShift(x,y,N)
+print('Optimal shift for RH: ', checkShiftRH)
+
+correlationFunction(x, y, timeInHours=checkShiftRH)
+print(correlationFunction(x, y, timeInHours=checkShiftRH))
+
+#We check a variable with very low correlation with the resulting CO (GT) variable
+x = airData.AH       # independent variable
+checkShiftAH = checkShift(x,y,N)
+print('Optimal shift for AH: ',checkShiftAH)
+
+correlationFunction(x, y, timeInHours=checkShiftAH)
+print(correlationFunction(x, y, timeInHours=checkShiftAH))
+
+#We check a variable with very low correlation with the resulting CO (GT) variable.
+
+x = airData['T']      # independent variable
+checkShiftT = checkShift(x,y,N)
+print('Optimal shift for T: ',checkShiftT)
+
+correlationFunction(x, y, timeInHours=checkShiftT)
+print(correlationFunction(x, y, timeInHours=checkShiftT))
+
+#preparing data for model
+airData2 = airData[['dateAndTime', 'CO(GT)','RH', 'T']]
+airData2['weather_time'] = airData2['dateAndTime']
+
+airData2.head(3)
+print(airData2.head(3))
+
+airData3 = shiftDataFrame12(airData2, 'weather_time', timeInHours=12)
+airData3.rename(columns={'weather_time':'Shift_weather_time'}, inplace=True)
+print("  ")
+#airData3.head(13)
+#print(airData3.head(13))
+
+airData4 = shiftDataFrame12(airData3, 'RH', timeInHours=12)
+airData4.rename(columns={'RH':'Shift_RH'}, inplace=True)
+
+airData5 = shiftDataFrame12(airData4, 'T', timeInHours=12)
+airData5.rename(columns={'T':'Shift_T'}, inplace=True)
+print("  airData5")
+airData5 = airData5.dropna(how ='any')
+airData5.head()
+print(airData5)
+
+
+airData5.plot(x='Shift_T', y='CO(GT)', style='o')
+plt.title('Shift_T vs CO')
+plt.xlabel('Shift_T')
+plt.ylabel('CO')
+plt.savefig("T_CO(GT).png")
+plt.show()
+
+airData5.plot(x='Shift_RH', y='CO(GT)', style='o')
+plt.title('Shift_RH vs CO')
+plt.xlabel('Shift_RH')
+plt.ylabel('CO')
+plt.savefig("RH_CO(GT).png")
+plt.show()
+
+#model building
+X = airData5[['Shift_RH', 'Shift_T']].values
+y = airData5['CO(GT)'].values
+
+# divide - training variables and test variables
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+trainRegression = LinearRegression()
+trainRegression.fit(X_train, y_train)
+
+LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False)
+
+yNew = trainRegression.predict(X_test)
+yNew = np.round(yNew, decimals=2)
+
+#with test variables
+dataFrameWithTestVariables = pd.DataFrame({'CO(GT) Actual': y_test, 'CO(GT)_Predicted': yNew})
+dataFrameWithTestVariables.head(5)
+
+dataFrameWithTestVariables.head(50).plot()
+plt.savefig("dataFrameWithTestVariables_CO(GT).png")
+plt.show()
+
+#info
+print('Mean Squared Error:     ', metrics.mean_squared_error(y_test, yNew))
+print('Mean Absolute Error:    ', metrics.mean_absolute_error(y_test, yNew))
+print('Median Absolute Error:  ', metrics.median_absolute_error(y_test, yNew))
+
+
+# Plots for month, weekDay and hours level of CO
+airData.pivot_table(index='month', values='CO(GT)', aggfunc='mean').plot(kind='bar')
+plt.savefig("month_CO(GT).png") #Save plot as a image - will be included in report
+airData.pivot_table(index='weekdayName', values='CO(GT)', aggfunc='mean').plot(kind='bar', color='g')
+plt.savefig("weekDay_CO(GT).png")
+airData.pivot_table(index='hours', values='CO(GT)', aggfunc='mean').plot(kind='bar', color='y')
+plt.savefig("hours_CO(GT).png")
+plt.show()
+
+# Plots for month, weekDay and hours level of NMHC
+airData.pivot_table(index='month', values='NMHC(GT)', aggfunc='mean').plot(kind='bar')
+plt.savefig("month_NMHC(GT).png") #Save plot as a image - will be included in report
+airData.pivot_table(index='weekdayName', values='NMHC(GT)', aggfunc='mean').plot(kind='bar', color='g')
+plt.savefig("weekDay_NMHC(GT).png")
+airData.pivot_table(index='hours', values='NMHC(GT)', aggfunc='mean').plot(kind='bar', color='y')
+plt.savefig("hours_NMHC(GT).png")
+plt.show()
+
+# Plots for month, weekDay and hours level of NO2
+airData.pivot_table(index='month', values='NO2(GT)', aggfunc='mean').plot(kind='bar')
+plt.savefig("month_NO2(GT).png") #Save plot as a image - will be included in report
+airData.pivot_table(index='weekdayName', values='NO2(GT)', aggfunc='mean').plot(kind='bar', color='g')
+plt.savefig("weekDay_NO2(GT).png")
+airData.pivot_table(index='hours', values='NO2(GT)', aggfunc='mean').plot(kind='bar', color='y')
+plt.savefig("hours_NO2(GT).png")
+plt.show()
